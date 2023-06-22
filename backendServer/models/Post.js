a0a17761f83f49f7121b16cf79698cee89954920
mansa-keita <sk22016033@utg.edu.gm>
2023-06-22 17:06:27 +0000

    first commit

---
+++ backendServer/models/Post.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize, DataTypes) => {
+
+    const Posts = sequelize.define("Posts", {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+         
+        },
+
+        PostText: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            
+        },
+
+
+        userName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          
+        }
+
+    })    
+   return Posts     
+
+}
---
+++ backendServer/routes/Posts.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const {Posts} = require("../models")
+
+const router = express.Router()
+
+router.get("/", async(req, res) =>{
+    const listOfPosts = await Posts.findAll()
+    res.json(listOfPosts)
+} )
+
+
+router.post("/", async(req, res)=>{
+    const post = req.body;
+    await Posts.create(post)
+    res.json(post)
+    
+})
+
+
+
+
+
+
+module.exports = router
